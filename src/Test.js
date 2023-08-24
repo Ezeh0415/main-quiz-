298923f73a24246ec461216b2ec17d9a0398ec6e
Geeeditor <anyanimedivine2000@gmail.com>
2023-08-24 16:09:54 -0700

    json file imported to test.js

--- src/Test.js
+++ src/Test.js
@@ -1,130 +1,112 @@
-import {useState, useEffect} from 'react'
+import jsonData from "./compononent/quiz.json";
+
+import { useState, useEffect } from "react";
 const Test = () => {
+  const [Question, setQuestion] = useState([]);
+  const [Answer, setCurrent] = useState([
+    {
+      questionAnswer: [
+        { questionTest: "Elon musk", isCorrect: true },
+        { questionTest: "Bill Gate", isCorrect: false },
+        { questionTest: "Mark zugarbeg", isCorrect: false },
+        { questionTest: "alico dangote", isCorrect: false },
+      ],
+    },
+    {
+      questionAnswer: [
+        { questionTest: "import (useState) from ./react", isCorrect: false },
+        { questionTest: "import [useState] from ./react", isCorrect: false },
+        { questionTest: "import useState from ./react", isCorrect: false },
+        { questionTest: "import {useState} from ./react", isCorrect: true },
+      ],
+    },
+    {
+      questionAnswer: [
+        { questionTest: "by adding a function inside", isCorrect: false },
+        { questionTest: "By useing timeout for it", isCorrect: false },
+        {
+          questionTest: "by adding dependence in the useEffect",
+          isCorrect: true,
+        },
+        { questionTest: "none of the above", isCorrect: false },
+      ],
+    },
+    {
+      questionAnswer: [
+        { questionTest: "uesEffect(() => {})", isCorrect: false },
+        { questionTest: "uesEffect(() => {},[])", isCorrect: true },
+        { questionTest: "uesEffect() => {})", isCorrect: false },
+        { questionTest: "uesEffect([], () => {})", isCorrect: false },
+      ],
+    },
+    {
+      questionAnswer: [
+        { questionTest: "create-react-app filename", isCorrect: false },
+        { questionTest: "react-app-create filename", isCorrect: false },
+        { questionTest: "npm create-react-app filename", isCorrect: false },
+        { questionTest: "npx create-react-app filename", isCorrect: true },
+      ],
+    },
+  ]);
+  const [isPending, setIsPending] = useState(true);
+  const [c, setCurrentQuestion] = useState(0);
+  const [ans, setCurrentAnswer] = useState(0);
 
-    const [Question, setQuestion ] = useState([]);
-    const [Answer, setCurrent] = useState([
-     {
-          questionAnswer:[
-                     { questionTest: 'Elon musk', isCorrect : true},
-                     { questionTest: 'Bill Gate', isCorrect : false},
-                     { questionTest: 'Mark zugarbeg', isCorrect : false},
-                     { questionTest: 'alico dangote', isCorrect : false}
-                  
-           ]
-     },
-     {
-          questionAnswer:[
-                     { questionTest: 'import (useState) from ./react', isCorrect : false},
-                     { questionTest: 'import [useState] from ./react', isCorrect : false},
-                     { questionTest: 'import useState from ./react', isCorrect : false},
-                     { questionTest: 'import {useState} from ./react', isCorrect : true}
-                     
-                   ]
-     },
-     {
-          questionAnswer:[
-                     { questionTest: 'by adding a function inside', isCorrect : false},
-                     { questionTest: 'By useing timeout for it', isCorrect : false},
-                     { questionTest: 'by adding dependence in the useEffect', isCorrect : true},
-                     { questionTest: 'none of the above', isCorrect : false}
-                   ]
-     },
-     {
-          questionAnswer:[
-                     { questionTest: 'uesEffect(() => {})', isCorrect : false},
-                     { questionTest: 'uesEffect(() => {},[])', isCorrect : true},
-                     { questionTest: 'uesEffect() => {})', isCorrect : false},
-                     { questionTest: 'uesEffect([], () => {})', isCorrect : false},
-                   ]
-     },
-     {
-          questionAnswer:[
-                     { questionTest: 'create-react-app filename', isCorrect : false},
-                     { questionTest: 'react-app-create filename', isCorrect : false},
-                     { questionTest: 'npm create-react-app filename', isCorrect : false},
-                     { questionTest: 'npx create-react-app filename', isCorrect : true},
-                   ]
-     }
-
-    ])
-    const [isPending, setIsPending] = useState(true);
-    const [c, setCurrentQuestion] = useState(0);
-    const [ans, setCurrentAnswer] = useState(0)
-
-
-    useEffect(() => {
-
-   setTimeout(() => {
-
-     fetch(` http://localhost:5000/question`)
-     .then( res => {
-          if(!res.ok) {
-               throw Error ('please check ur network and try again')
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(jsonData)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("please check ur network and try again");
           }
           return res.json();
-     })
-     .then( data => {
-          setQuestion(data)
+        })
+        .then((data) => {
+          setQuestion(data);
           setIsPending(false);
-     })
-     .catch(err) {
-          console.log(err.mesage);
-     }
+        })
+        .catch((err) => {
+          // console.log(err.mesage);
+        });
+    }, 1000);
+  }, []);
 
-   }, 1000);
-   
-    }, [] );
+  const handleClick = () => {
+    const nextquest = c + 1;
+    const nextans = ans + 1;
+    if (nextquest < Question.length && nextans < Answer.length) {
+      setCurrentQuestion(nextquest);
+      setCurrentAnswer(nextans);
+    } else {
+      alert("u don reach");
+    }
+  };
 
-   
+  const handleReset = () => {
+    setCurrentQuestion(0);
+    setCurrentAnswer(0);
+  };
 
-   const handleClick = () => {
-     const nextquest = c + 1;
-     const nextans = ans + 1;
-     if (nextquest < Question.length && nextans < Answer.length) {
-          setCurrentQuestion(nextquest);
-          setCurrentAnswer(nextans)
-     } else {
-          alert('u don reach')
-     }
-   }
+  //Destructuring Assignmnent
 
-   const handleReset = () => {
-     setCurrentQuestion(0);
-     setCurrentAnswer(0)
-   }
+  const [questionOne] = [jsonData];
 
-//    const animals =  [ {animal:'cat', owner:'buchi'},
-//    {animal:'dog', owner:'messo'},  {animal:'bird', owner:'ozy'} ] ;
-
-//    const [animalOne, animalTwo, animalThree] = animals;
-
-//    const{animal, owner} = animalOne;
-
-//    console.log(animalOne)
-
-   const [questionAnswer] = Question;
-
-   console.log(questionAnswer);
-
-
-    return ( 
+  return (
+    <div>
+      {/* {isPending && <h2>loading...</h2>} */}
+      {/* {console.log(questionData)} */}
+      <h1>{/* {questionText} */}</h1>
+      {Question && (
         <div>
-               {isPending && <h2>loading...</h2>}
-               {Question && (
-                    <div>
-                        <div>
-                          {/* <h1>{questionText}</h1> */}
-                        </div>
-                    </div>
-                   
-               )};
-               
-               <button onClick={handleClick}>click me</button>
-               <button onClick={handleReset}>Reset me</button>
-         
-
+          <div>
+            <h1>{/* {questionAnswer.questionTest} */}</h1>
           </div>
-     );
-}
- 
+        </div>
+      )}
+      ;<button onClick={handleClick}>click me</button>
+      <button onClick={handleReset}>Reset me</button>
+    </div>
+  );
+};
+
 export default Test;
